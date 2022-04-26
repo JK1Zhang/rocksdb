6929f28924192dff20e66a0d451eb860905bbeae
JK1Zhang <zjk1998@mail.ustc.edu.cn>
2022-04-26 10:48:04 +0800

    uni_index.h init

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # UniKV atop RocksDB DevLog
 
-[TOC]
+- [UniKV atop RocksDB DevLog](#unikv-atop-rocksdb-devlog)
+    - [[done]增加level0文件数量](#done增加level0文件数量)
+    - [[todo]索引结构定义](#todo索引结构定义)
+    - [[todo]索引接口实现](#todo索引接口实现)
+    - [[todo]接口嵌入](#todo接口嵌入)
 
 
 
 ### [done]增加level0文件数量
 
 由参数 level0_file_num_compaction_trigger 控制
-
 file path: rocksdb/include/rocksdb/options.h
 
 ```
@@ -20,6 +23,8 @@ change: modified to 40
 
 
 ### [todo]索引结构定义
+file path: rocksdb/include/rocksdb/uni_index.h (new created)
+
 
 ### [todo]索引接口实现
 
---
+++ examples/test_all.sh
@@ -0,0 +1,16 @@
+echo 'c_simple_example---------------------'
+./c_simple_example
+echo 'compaction_filter_example------------'
+./compaction_filter_example 
+echo 'compact_files_example----------------'
+./compact_files_example
+echo 'column_families_example--------------'
+./column_families_example
+echo 'optimistic_transaction_example-------'
+./optimistic_transaction_example
+echo 'options_file_example-----------------'
+./options_file_example
+echo 'simple_example-----------------------'
+./simple_example
+echo 'transaction_example------------------'
+./transaction_example
